115e90a48c49db6435cf698f7a1f38ef70fb36f3
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-04-26 19:27:38 -0400

    added scheduleUpdate function to change CSS as time changes

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+//moment.js for date and time in header
 var today = moment().format('dddd, MMMM Do YYYY, h:mm a');
 var currentDay = document.getElementById('currentDay')
 currentDay.textContent = today;
 
+function scheduleUpdate() {
+    var currentTime = moment().hours();
+    console.log('hours', currentTime);
+
+    for (let i = 6; i < 20; i++) {
+        var textArea = $('#timeSlot-'+i)
+        if (currentTime > i){
+            $(textArea).addClass('past');
+        }else if (currentTime === i){
+            $(textArea).addClass('present');
+        }else {
+            $(textArea).addClass('future');
+        }
+    }
+}
+scheduleUpdate();
