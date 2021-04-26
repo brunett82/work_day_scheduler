5555328a8781763f68f80d42c574ed2b92358be6
Rob Brunett <rb3@Robs-Air.attlocal.net>
2021-04-26 18:39:18 -0400

    added moment logic to script.js to display date and time in id = currentDay

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-var today = new Date();
-console.log("date", today);
-$()
+var today = moment().format('dddd, MMMM Do YYYY, h:mm a');
+var currentDay = document.getElementById('currentDay')
+currentDay.textContent = today;
+
